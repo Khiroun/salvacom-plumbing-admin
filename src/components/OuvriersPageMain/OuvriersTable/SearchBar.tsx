61b0301edb7c4b1aba82e60aed15c38d4b1ad8a4
Khiroun <em_khiroun@esi.dz>
2022-07-15 17:30:42 +0100

    Afficher la liste des ouvriers

---
+++ src/components/OuvriersPageMain/OuvriersTable/SearchBar.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import { FaSearch } from "react-icons/fa";
+
+export default function SearchBar() {
+  return (
+    <Paper
+      component="form"
+      sx={{
+        p: "2px 4px",
+        display: "flex",
+        alignItems: "center",
+        width: "100%",
+      }}
+    >
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Rechercher"
+        inputProps={{ "aria-label": "search google maps" }}
+      />
+      <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+        <FaSearch />
+      </IconButton>
+      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+    </Paper>
+  );
+}
--- src/components/OuvriersPageMain/OuvriersTable/index.tsx
+++ src/components/OuvriersPageMain/OuvriersTable/index.tsx
@@ -1,7 +1,57 @@
-import React from "react";
-
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import { getAll } from "../../../firebase";
+import SearchBar from "./SearchBar";
 const OuvriersTable = () => {
-  return <div>OuvriersTable</div>;
+  const [ouvriers, setOuvriers] = useState([]);
+  useEffect(() => {
+    getAll("ouvriers").then((res) => {
+      setOuvriers(res);
+    });
+  }, []);
+  return (
+    <Paper>
+      <SearchBar />
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Nom</TableCell>
+              <TableCell>Prénom</TableCell>
+              <TableCell>Description</TableCell>
+              <TableCell>email</TableCell>
+              <TableCell>Téléphone</TableCell>
+              <TableCell>Adresse</TableCell>
+              <TableCell>Mot de passe</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {ouvriers.map((ouvrier) => {
+              return (
+                <TableRow key={ouvrier.id}>
+                  <TableCell>{ouvrier.lastName}</TableCell>
+                  <TableCell>{ouvrier.firstName}</TableCell>
+                  <TableCell>{ouvrier.description}</TableCell>
+                  <TableCell>{ouvrier.email}</TableCell>
+                  <TableCell>{ouvrier.phone}</TableCell>
+                  <TableCell>{ouvrier.address}</TableCell>
+                  <TableCell>{ouvrier.password}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
+  );
 };
 
 export default OuvriersTable;
